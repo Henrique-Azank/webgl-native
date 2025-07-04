d14185cf6dd05ccf6596a94db887e4605379d484
Henrique Azank dos Santos <102073359+Henrique-Azank@users.noreply.github.com>
2025-07-04 18:40:14 -0300

    feat: added jsx support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,17 @@ import Styles from "./page.module.scss";
 export default function Page() {
   return (
     <div>
-      <h1 className={Styles.Text}>Hello, Next.js!</h1>
+      {/* Examples navigator */}
+      <div>
+        {/* Example 1 - Basic rendering of a 2D object using pure WebGL */}
+        <div>
+          <h1>Basic 2D rendering using WebGL</h1>
+        </div>
+        {/* Example 2 - Basic rendering of a 3D object using pure WebGL*/}
+        <div>
+          <h1>Basic 3D rendering using WebGL</h1>
+        </div>
+      </div>
     </div>
   );
 }
